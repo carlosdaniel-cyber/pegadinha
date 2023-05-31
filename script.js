643ca2ba1a1a4d6371bf469aa2695a5682e92f3e
Carlos Daniel Barros Pereira <carlosdanielbp101@gmail.com>
2023-05-31 15:33:34 -0300

    Substitui o transform usado para centralizar a div e configura o posicionamento do botão

--- script.js
+++ script.js
@@ -19,21 +19,21 @@ window.addEventListener('mousemove', (event) => {
     let distance = calculateDistance(mousePos.x, buttonPos.x, mousePos.y, buttonPos.y)
     console.log(mousePos)
 
-    const winWidth = window.innerWidth;
-    const winHeight = window.innerHeight;
+    const windowWidth = window.innerWidth * 0.9;
+    const windowHeight = window.innerHeight * 0.9;
 
     // function that returns a random number between a min and max
     function getRandomNumber(min, max) {
       return Math.random() * (max - min) + min; 
     }
 
-    let randomTop = getRandomNumber(0, winHeight);
-    let randomLeft = getRandomNumber(0, winWidth);
+    let randomTop = getRandomNumber(0, windowHeight);
+    let randomLeft = getRandomNumber(0, windowWidth);
 
-    if (distance <= 50) {
-      denialButton.style.position = 'fixed'
-      denialButton.style.position = randomTop +"px";
-      denialButton.style.top = randomLeft +"px";
+    if (distance <= 70) {
+      denialButton.style.position = "fixed"
+      denialButton.style.top = randomTop +"px";
+      denialButton.style.left = randomLeft +"px";
       }
   });
   
